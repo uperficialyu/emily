f7f0daff3f9e461c37aa010e68bd26d05510c565
Email <947201766@qq.com>
2021-07-18 09:11:37 +0800

    脚手架搭好

--- pc脚手架4/src/index.tsx
+++ pc脚手架4/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Test111 from './components/Test.tsx';
+import Test111 from './components/Test/Test'
 
 ReactDOM.render(
   <Test111 />, document.getElementById('root')
